24a1c75f1636fdafacd3f1462334c710df07b6d5
Li Keqing <auroracloud4096@gmail.com>
2019-07-31 14:35:46 +0800

    C++ has builtin bool type, just ignore it

--- bool.h
+++ bool.h
@@ -1,8 +1,12 @@
 #ifndef bool_h
 #define bool_h
 
+#if !defined(__cplusplus)
+
 typedef typeof(0 == 0) bool;
 const bool static true = 0 == 0;
 const bool static false = 0 != 0;
+
+#endif
 
 #endif /* bool_h */
